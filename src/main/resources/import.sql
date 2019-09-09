21759d1e8cef2c1679305159094fc2ca36c22ac2
Alumno <Alumno@LAB1504-25>
2019-09-09 12:51:28 -0500

    Second Commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(1,'Tecnologia','USB',15,150);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(2,'Tecnologia','Laptop',80,1500);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(3,'Tecnologia','Disco Duro',140,342);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(4,'Tecnologia','TV',340,2000);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(5,'Tecnologia','Teclado',26,100);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(6,'Tecnologia','Mouse',130,300);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(7,'Tecnologia','Monitor',14,1400);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(8,'Tecnologia','Microfono',42,240);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(9,'Tecnologia','Camara',18,420);
+INSERT INTO productos(id_producto, pro_categoria,pro_name,pro_precio,pro_stock) VALUES(10,'Tecnologia','Memoria Ram',40,500);
+
